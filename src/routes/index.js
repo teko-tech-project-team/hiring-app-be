265f07c4835798ad5293f10ca3a40cda8346ba0e
Mohammad Yasfiq <86519218+Yasfiq@users.noreply.github.com>
2023-02-04 18:12:43 +0700

    Update index add jobseeker router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,19 @@ const router = express()
 
 const recruiterRoute = require('./recruiter.route')
 const authRecruiterRoute = require('./authRecruiter.route')
+const jobseekerRoute = require("./jobseeker.route");
+const authJobseekerRoute = require("./authJobseeker.route");
 
 // routing landing page
 router.get('/', (req, res)=> {
     return res.send("Backend successfully running at landing page")
 })
+
+// routing profile
+router.use("/jobseeker", jobseekerRoute);
+
+// routinng auth
+router.use("/auth", authJobseekerRoute);
 
 // routing recruiter
 router.use('/recruiter', recruiterRoute)
